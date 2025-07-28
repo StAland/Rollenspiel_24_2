48b49444fd51448a4066cf8b810a9c967120d12e
Tobias Fuhrmann <Tobias.Fuhrmann@futuristen.education>
2025-07-28 12:07:41 +0200

    Added new classes for Charakter, Spieler, Gegner

---
+++ Rollenspiel_24_2/Charakter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rollenspiel_24_2
+{
+    internal class Charakter
+    {
+    }
+}
