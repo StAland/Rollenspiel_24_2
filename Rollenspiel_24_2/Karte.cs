daf43f35dffa3fb8d816d64101ab6b469d9d4450
StAland <steffen.aland@gmx.de>
2025-07-28 13:00:16 +0200

    Zufallskarte erstellen

--- Rollenspiel_24_2/Karte.cs
+++ Rollenspiel_24_2/Karte.cs
@@ -9,5 +9,28 @@ namespace Rollenspiel_24_2
     internal class Karte
     {
         private Feld[,] felder;
+
+
+        public Karte()
+        {
+            // ToDo Karte aus Json File oder ähnlichem laden
+            CreateRandomMap();
+        }
+
+        private void CreateRandomMap()
+        {
+            int x = 10;
+            int y = 10;
+            felder = new Feld[x, y];
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    var rand = new Random();
+                    var begehbar = rand.Next(0, 2) == 1;
+                    felder[i, j] = new Feld("Feldname", begehbar);
+                }
+            }
+        }
     }
 }
