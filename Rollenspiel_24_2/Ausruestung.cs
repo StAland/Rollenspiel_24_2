0d52b8aa91d1af2aa646b4f0170054f66b0ab559
LaPipe <studio_lapipe@proton.me>
2025-07-29 10:46:39 +0200

    1ter Versuch

--- Rollenspiel_24_2/Ausruestung.cs
+++ Rollenspiel_24_2/Ausruestung.cs
@@ -6,7 +6,61 @@ using System.Threading.Tasks;
 
 namespace Rollenspiel_24_2
 {
-    public class Ausruestung
+    public class Ausruestung : Gegenstand
     {
+        private int angriffswert;
+        private int verteidigungswert;
+
+        public Ausruestung(string name, int angriff, int verteidigung) 
+            : base(name, "Ausruestung")
+        {
+            this.angriffswert = angriff;
+            this.verteidigungswert = verteidigung;
+        }
+
+        public int getAngriffswert()
+        {
+            return angriffswert;
+        }
+
+        public int getVerteidigungswert()
+        {
+            return verteidigungswert;
+        }
+
+        public override void benutzen()
+        {
+            Console.WriteLine($"{getName()} ausgerüstet");
+            if (angriffswert > 0)
+                Console.WriteLine($"Angriffswert: +{angriffswert}");
+            if (verteidigungswert > 0)
+                Console.WriteLine($"Verteidigungswert: +{verteidigungswert}");
+        }
+    }
+
+    public class Schwert : Ausruestung
+    {
+        public Schwert() : base("Schwert", 7, 0)
+        {
+        }
+
+        public override void benutzen()
+        {
+            Console.WriteLine("Schwert ausgerüstet");
+            base.benutzen();
+        }
+    }
+
+    public class Schild : Ausruestung
+    {
+        public Schild() : base("Schild", 0, 5)
+        {
+        }
+
+        public override void benutzen()
+        {
+            Console.WriteLine("Schild ausgerüstet");
+            base.benutzen();
+        }
     }
 }
--- Rollenspiel_24_2/Verbrauchsgegenstand.cs
+++ Rollenspiel_24_2/Verbrauchsgegenstand.cs
@@ -6,7 +6,61 @@ using System.Threading.Tasks;
 
 namespace Rollenspiel_24_2
 {
-    public class Verbrauchsgegenstand
+    public class Verbrauchsgegenstand : Gegenstand
     {
+        private int leben;
+        private int mana;
+
+        public Verbrauchsgegenstand(string name, int leben, int mana) 
+            : base(name, "Verbrauchsgegenstand")
+        {
+            this.leben = leben;
+            this.mana = mana;
+        }
+
+        public int getLeben()
+        {
+            return leben;
+        }
+
+        public int getMana()
+        {
+            return mana;
+        }
+
+        public override void benutzen()
+        {
+            Console.WriteLine($"{getName()} wurde verwendet!");
+            if (leben > 0)
+                Console.WriteLine($"Leben wurde um {leben} erhöht.");
+            if (mana > 0)
+                Console.WriteLine($"Mana wurde um {mana} erhöht.");
+        }
+    }
+
+    public class Heiltrank : Verbrauchsgegenstand
+    {
+        public Heiltrank() : base("Heiltrank +10", 10, 0)
+        {
+        }
+
+        public override void benutzen()
+        {
+            Console.WriteLine("+10 Leben");
+            base.benutzen();
+        }
+    }
+
+    public class Manatrank : Verbrauchsgegenstand
+    {
+        public Manatrank() : base("Manatrank +10", 0, 10)
+        {
+        }
+
+        public override void benutzen()
+        {
+            Console.WriteLine("+10 Mana");
+            base.benutzen();
+        }
     }
 }
