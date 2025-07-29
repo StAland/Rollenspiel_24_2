4b834ab35f0a73c796c0c4c1395a0b3c5d6e6423
LaPipe <studio_lapipe@proton.me>
2025-07-29 10:47:16 +0200

    Gegenstand

--- Rollenspiel_24_2/Gegenstand.cs
+++ Rollenspiel_24_2/Gegenstand.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace Rollenspiel_24_2
 {
-    public class Gegenstand
+
+    public abstract class Gegenstand
     {
+        private string name;
+        private string typ;
+
+        public Gegenstand(string name, string typ)
+        {
+            this.name = name;
+            this.typ = typ;
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public string getTyp()
+        {
+            return typ;
+        }
+
+        public abstract void benutzen();
     }
 }
