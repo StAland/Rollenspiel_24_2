72d9794e57e9c716f12aee1680685eff0b57cc84
FuturePagan <Patrick.Boehme@futuristen.education>
2025-07-30 11:50:32 +0200

    Interface Karte

---
+++ Logik/IKarte.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Logik.Karte;
+
+namespace Logik
+{
+    public interface IKarte
+    {
+        public enum Richtung;
+        public bool IsBetretbar(Point point);
+        public void Bewegen(Richtung richtung);
+
+    }
+}
