f7e0157862601f27404b30df928b288a4c04e0c1
LaPipe <159536765+LaPipe-CGN@users.noreply.github.com>
2025-07-31 23:53:06 +0200

    InventarForm.Designer.cs

--- Rollenspiel_24_2/InventarForm.Designer.cs
+++ Rollenspiel_24_2/InventarForm.Designer.cs
@@ -2,15 +2,8 @@ namespace Rollenspiel_24_2
 {
     partial class InventarForm
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,16 +15,10 @@ namespace Rollenspiel_24_2
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
             SuspendLayout();
-            // 
-            // InventarForm
-            // 
+
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(400, 300);
